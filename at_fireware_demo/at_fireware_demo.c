540b5206006d9d76a477f5f8e60ef150046d9527
信勇哥得水牛 <517661395@qq.com>
2019-01-15 19:30:01 +0800

    modified:   at_fireware_demo/at_fireware_demo.c

--- at_fireware_demo/at_fireware_demo.c
+++ at_fireware_demo/at_fireware_demo.c
@@ -13,8 +13,8 @@ unsigned char Char2Num(int n){
 }
 //只支持2个字节，把字符串数字转换int数值
 int GetLen(unsigned char h,unsigned char l){
-	int hn = Num2Char(h);
-	int ln = Num2Char(l);
+	int hn = Num2Char(h); //高位
+	int ln = Num2Char(l); //低位
 	if(hn == -1 && ln == -1 ){
 		return -1;
 	}else if(hn == -1){
@@ -28,11 +28,11 @@ int GetLen(unsigned char h,unsigned char l){
 //只支持2个字节，把int数值转换为字符串数字
 void ConventLen(int len,unsigned char *data){
 	if(len<10){
-		data[0] = '0';
-		data[1] = Char2Num(len);
+		data[0] = '0'; //低位
+		data[1] = Char2Num(len); //高位
 	}else{
-		data[0] = Char2Num(len / 10 % 10);
-		data[1] = Char2Num(len / 1 % 10);
+		data[0] = Char2Num(len / 10 % 10); //低位
+		data[1] = Char2Num(len / 1 % 10); //高位
 	}
 	return ;
 }
@@ -60,8 +60,8 @@ unsigned char HexStr2HexNum(unsigned char left,unsigned char right){
 }
 //hexnum 转换为 hexstring
 void HexNum2HexStr(unsigned char hexnum,unsigned char* hexstr){
-	hexstr[0] = hn2hc( hexnum >> 4 );
-	hexstr[1] = hn2hc( hexnum & 0x0F );
+	hexstr[0] = hn2hc( hexnum >> 4 ); //低位
+	hexstr[1] = hn2hc( hexnum & 0x0F ); //高位
 	return ;
 }
 //匹配帧 数据
@@ -84,8 +84,8 @@ enum ModuleStateEnum{
 enum ModuleStateEnum modulestate=PowerOnS;
 //串口发送数据指令帧
 //#define ATWDS 12
-unsigned char tATWPPID[]="AT+WPPID=\"<PID>\"\r";
-unsigned char tATWPPK[]="AT+WPPK=\"b784118c-a9bf-4720-81f5-4dc7154cd23a\"\r";
+unsigned char tATWPPID[]="AT+WPPID=\"00000000000000000000000000000000\"\r";   // 注：需要从对接交付文档/平台控制台中获取并”替换掉“PID
+unsigned char tATWPPK[]="AT+WPPK=\"00000000-0000-0000-0000-000000000000\"\r"; // 注：需要从对接交付文档/平台控制台中获取并”替换掉“PKEY
 unsigned char tATWCSAS[]="AT+WCS=\"AIRKISS\",600,true\r";
 unsigned char tATWCSAP[]="AT+WCS=\"AP\",600,true\r";
 unsigned char tATWCC[]="AT+WCC\r";
@@ -115,42 +115,47 @@ unsigned char rxData[RXLEN]="\r\n+WDR=05,\"0001000200\"\r\n"	;
 unsigned char tlvValue[8];//暂定长度，有需要自己调整
 
 int RevData(){
+	//计算  \r\n+WDR=05,到底是05还是5，通过判断 rxData[8] ',' 判断
 	unsigned short baseline =   (rxData[8] == ',' ? 0 : 1)  ;
+	//计算 TLV 里面的tag 4个uchar，实际为2个字节给用户使用
 	unsigned short tag = (HexStr2HexNum(rxData[10+baseline] , rxData[11+baseline])<<8)|
 											(HexStr2HexNum(rxData[12+baseline] , rxData[13+baseline])&0xff);
+	//计算 TLV 里面的len  4个uchar，实际为2个字节给用户使用
 	unsigned short len = (HexStr2HexNum(rxData[14+baseline] , rxData[15+baseline])<<8)|
 											(HexStr2HexNum(rxData[16+baseline] , rxData[17+baseline])&0xff);
+  //计算 TLV 里面的value，value长度是TLV 的L决定的
 	int i=0;
 	for(;i<len;i++){
 		tlvValue[i] = HexStr2HexNum(rxData[i*2+18+baseline] , rxData[i*2+19+baseline]);
 	}
+	//调用 RevDataProcess提供用户使用
 	i = RevDataProcess( tag, len, tlvValue );
 	return i;
 }
 //设置发送云端的TLV数据
 int SetSendData(unsigned short tag,unsigned short length,unsigned char* value){
+	// atlen 是计算 AT+WDS=<len> 指令的len长度，TLV的TL为4个字节，另外加value的长度
 	unsigned short atlen = 4 + length;
 	unsigned char atData[2];//复用数据
 	int loop=0;
 	//ConventLen(atlen,atData);
 	//tATWDS[7] = atData[0];
 	//tATWDS[8] = atData[1];
+	//计算TAG数值
 	HexNum2HexStr(tag >> 8 ,atData);
 	tATWDS[11] = atData[0];
 	tATWDS[12] = atData[1];
-
 	HexNum2HexStr(tag & 0xff ,atData);
 	tATWDS[13] = atData[0];
 	tATWDS[14] = atData[1];
-
+	//计算Length数值
 	HexNum2HexStr(length >> 8 ,atData);
 	tATWDS[15] = atData[0];
 	tATWDS[16] = atData[1];
-
 	HexNum2HexStr(length & 0xff ,atData);
 	tATWDS[17] = atData[0];
 	tATWDS[18] = atData[1];
-
+	//计算Value数值
 	for(;loop<length;loop++){
 		HexNum2HexStr(value[loop],atData);
 		tATWDS[19+loop*2]=atData[0];
@@ -166,23 +171,23 @@ void ModuleCompleteDataProcess(){
 		return;
 	}
 	if(MatchCommand( rATWIFICONN,rxData,21)){
-		//Wi-Fi连接完成
+		//Wi-Fi连接完成，客户可填充自己需要的代码，例如指示灯
 		return;
 	}
 	if(MatchCommand( rATWIFIDISCONN,rxData,24)){
-		//Wi-Fi连接失败
+		//Wi-Fi连接失败，客户可填充自己需要的代码，例如指示灯
 		return;
 	}
 	if(MatchCommand( rATCLOUDCONN,rxData,22)){
-		//云端连接完成
+		//云端连接完成，客户可填充自己需要的代码，例如指示灯
 		return;
 	}
 	if(MatchCommand( rATCLOUDDISCONN,rxData,25)){
-		//云端连接失败
+		//云端连接失败，客户可填充自己需要的代码，例如指示灯
 		return;
 	}
 	if(MatchCommand( rATWCSTIMEOUT,rxData,16)){
-		//配网失败
+		//配网失败，客户可填充自己需要的代码，例如指示灯
 		return;
 	}
 	/*
@@ -198,14 +203,16 @@ void ModuleCompleteDataProcess(){
 }
 void ModuleProcess(){
 	//if(modulestate == PowerOnS ){
-	//考虑模块有可能自动OTA 或者自动复位
+	//考虑模块有可能自动OTA 或者自动复位，匹配到ready指令
 	if(MatchCommand( rATREADY,rxData,9)){
 		rxSendData(tATWPPID,44);
 		modulestate=PidS;
 		return;
 	}
 	//}
+	//模块当下状态为发送完成PID
 	if(modulestate == PidS){
+		//等待反馈OK指令，如果匹配成功OK指令，则发送PK，否则重发PID
 		if(MatchCommand( rATOK,rxData,6)){
 			rxSendData(tATWPPK,47);
 			modulestate=PkS;
@@ -214,7 +221,9 @@ void ModuleProcess(){
 		}
 		return;
 	}
+	//模块当下状态为发送完成PK
 	if(modulestate == PkS){
+		//等待反馈OK指令，如果匹配成功OK指令，模块设置完成，否则重发PK
 		if(MatchCommand( rATOK,rxData,6)){
 			modulestate=CompleteS;
 		}else{
@@ -247,7 +256,9 @@ enum CFMStateEnum cfm=FRAME_OVER;
 int CommandFrameProcess(){
 	int frameSize = 0;
 	rxData[serialSkip] = readSerialByte();
+	//当前帧是结束状态，则开启下一轮帧头匹配
 	if(cfm==FRAME_OVER ){
+		//匹配到 帧头第一个字节
 		if(rxData[serialSkip] == 0x0d){
 			cfm = FRAME_HEADER_MATCHED;
 		}else{
@@ -255,21 +266,26 @@ int CommandFrameProcess(){
 			return 0;
 		}
 	}else if(cfm == FRAME_HEADER_MATCHED ){
+		//已经匹配到帧头第一个字节，需要判断第二个字节，如果成功则进入帧payload阶段
 		if(rxData[serialSkip] == 0x0a){
 			cfm = FRAME_BODY;
 		}else if(rxData[serialSkip] == 0x0d){
+			//发现第二个字节仍然是 0x0d，回退处理，需要若错处理 \r\r\n data \r\n 此类帧数据
 			rxData[serialSkip-1] = 0x0d;
 			return 0;
 		}else{
+			//第二个字节匹配不到 0x0a，因此状态结束退出从头再匹配
 			cfm = FRAME_OVER;
 			serialSkip = 0;
 			return 0;
 		}
 	}else if(cfm == FRAME_BODY ){
+		// 当前状态是在payload 处理阶段，发现  0x0d 可能是帧尾开始，进入FRAME_TAIL1_MATCHED状态
 		if(rxData[serialSkip] == 0x0d){
 			cfm = FRAME_TAIL1_MATCHED;
 		}
 	}else if(cfm == FRAME_TAIL1_MATCHED){
+		//判断0x0d 之后是否 0x0a，如果是，则进入帧尾结束
 		if(rxData[serialSkip] == 0x0a){
 			frameSize = serialSkip+1;
 			//发现帧数据小于6做容错处理
@@ -284,6 +300,7 @@ int CommandFrameProcess(){
 			cfm = FRAME_OVER;
 			return frameSize;
 		}else if(rxData[serialSkip] != 0x0d){
+			//发现数据并非 0x0d 则回到 payload处理状态，容错处理 \r\n data \r\r\n
 			cfm = FRAME_BODY;
 		}
 	}
